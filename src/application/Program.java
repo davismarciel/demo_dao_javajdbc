53b34965b2ed70bd63d1e4ec4a60616cc3946384
Davi Sá <davisdevi05@gmail.com>
2024-09-27 13:38:18 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -12,6 +14,8 @@ public class Program {
 		System.out.println(dep);
 		
 		Seller seller = new Seller(1, "Davi", "davi@email.com", new Date(), 3000.0, dep);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		System.out.println(seller);
 	}
